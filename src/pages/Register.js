f10bdb412a36fc6e47ad3693fa6ac07c5cbd90c7
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-09-11 14:38:21 +0500

    asdasd

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,62 +1,75 @@
 import React from 'react'
-import Img1 from "../1.jpg"
 
 function Register() {
     const style = {
-        register: "bg-grey-lighter min-h-screen flex flex-col"
+        register: "bg-grey-lighter min-h-screen flex flex-col",
+        register_child: "container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2",
+        register_container: "bg-white px-6 py-8 rounded shadow-md text-black w-full",
+        signuph1: "mb-8 text-3xl text-center",
+        input: "block border border-grey-light w-full p-3 rounded mb-4",
+        inputwrap: "h-[50px]  relative rounded-md  w-full bg-blue-400",
+        inputfile: "w-full h-full opacity-0 cursor-pointer bg-none",
+        inputh1: "absolute top-3 text-xl left-[20px] text-white z-10",
+        btn: "w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1 border hover:bg-red-300 border-black",
+        footer: "text-center text-sm text-grey-dark mt-4",
+        footer_link: "no-underline border-b border-grey-dark text-grey-dark",
+        footer_login: "text-grey-dark mt-6"
+
     }
+
     return (
         <div>
             <div className={style.register}>
-                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                        <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+                <div className={style.register_child}>
+                    <div className={style.register_container}>
+                        <h1 className={style.signuph1}>Sign up</h1>
                         <input
                             type="text"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            className={style.input}
                             name="fullname"
                             placeholder="Full Name" />
 
                         <input
                             type="text"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            className={style.input}
                             name="email"
                             placeholder="Email" />
 
                         <input
                             type="password"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            className={style.input}
                             name="password"
                             placeholder="Password" />
                         <input
                             type="password"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            className={style.input}
                             name="confirm_password"
                             placeholder="Confirm Password" />
-                        <div className=" h-[50px]  relative rounded-md  w-full bg-blue-400">
-                            <input type="file" className='w-full h-full opacity-0 cursor-pointer bg-none' />
-                            <h1 className='absolute top-3 text-xl left-[20px] text-white z-10'>Rasm tanlang!</h1>
+                        <div className={style.inputwrap}>
+                            <input type="file" className={style.inputfile} />
+                            <h1 className={style.inputh1}>Rasm tanlang!</h1>
                         </div>
+
 
                         <button
                             type="submit"
-                            className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                            className={style.btn}
                         >Create Account</button>
 
-                        <div className="text-center text-sm text-grey-dark mt-4">
+                        <div className={style.footer}>
                             By signing up, you agree to the
-                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            <a className={style.footer_link} href="#">
                                 Terms of Service
                             </a> and
-                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            <a className={style.footer_link} href="#">
                                 Privacy Policy
                             </a>
                         </div>
                     </div>
 
-                    <div className="text-grey-dark mt-6">
+                    <div className={style.footer_login}>
                         Already have an account?
-                        <a className="no-underline border-b border-blue text-blue" href="../login/">
+                        <a className={style.footer_link} href="../login/">
                             Log in
                         </a>.
                     </div>
